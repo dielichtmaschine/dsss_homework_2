d68fef556c980ca3b454791400b390878333fa0c
dielichtmaschine <132780058+dielichtmaschine@users.noreply.github.com>
2023-11-08 16:27:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-# Setup
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="math",
+    version="0.1",
+    author="Torsten Licht",
+    author_email="torsten.licht@fau.de",
+    packages=find_packages(),
+    install_requires=[],
+)
